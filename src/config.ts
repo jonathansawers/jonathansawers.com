12d696b1ea7468ac6ee3e32a91bd5a17c5c2e7ef
Jonathan Sawers <jonathansawers@icloud.com>
2026-02-18 16:06:42 +1030

    Add repo to Footer

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 export const siteConfig = {
   name: 'Jonathan Sawers',
   resume: 'resume.pdf',
+  repo: 'https://github.com/jonathansawers/jonathansawers.com',
   social: {
     linkedin: 'https://www.linkedin.com/in/jonathansawers',
   },
